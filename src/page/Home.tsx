fec56dd6b99a96d1a89a0bb02225d2fa6b6b2dd6
KazuyaMakino <101450293+Glen-Maki@users.noreply.github.com>
2023-01-17 21:03:47 +0900

    feat: 設定時刻一覧のページ実装

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,27 +1,27 @@
 import { useEffect, useState } from "react";
-
-type TableValues = {
-  time: string[];
-};
+import config from "../config";
 
 export const Home = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [message, setMessage] = useState<string | undefined>(undefined);
-  const [tableValue, setTableValue] = useState<TableValues | undefined>(
-    undefined
-  );
+  const [tableValue, setTableValue] = useState<string[] | undefined>(undefined);
 
-  const testUrl = "";
-  const url = "http://192.168.11.59:8080";
+  const [onDelete, setOnDelete] = useState<number>(0);
+
+  const url = config.URL;
 
   useEffect(() => {
-    // 送信の処理
-    fetch(testUrl + "/setting")
-      .then((response) => {
-        // return response.json();
+    // 設定時間の取得
+    fetch(url + "/confirm")
+      .then((res) => {
+        if (!res?.ok) {
+          setMessage("サーバーエラーです");
+        }
+        return res.json();
       })
-      .then((data) => {
-        // setTableValue();
+      .then((res) => {
+        console.log(res);
+        setTableValue(res);
       })
       .catch((e) => {
         setMessage("受信に失敗しました");
@@ -29,27 +29,62 @@ export const Home = () => {
       .finally(() => {
         setIsLoading(false);
       });
-  }, []);
+  }, [onDelete, url]);
+
+  const onButton = (index: number) => {
+    console.log(index);
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        index: index,
+      }),
+    };
+
+    // 送信の処理
+    fetch(url + "/delete", requestOptions)
+      .then((res) => {
+        if (!res?.ok) {
+          setMessage("サーバーエラーです");
+        }
+        setMessage(`削除に成功しました!`);
+      })
+      .catch((e) => {
+        setMessage("送信に失敗しました");
+      })
+      .finally(() => {
+        setIsLoading(false);
+        // FIXME: 非効率的
+        setOnDelete((e) => e + 1);
+      });
+  };
 
   return (
     <div className="flex h-full w-full flex-col items-center gap-4">
-      <table className="mt-5 w-24 table-auto text-lg">
+      <table className="mt-5 table-auto text-lg">
         <thead className="">
           <tr className="border-2 p-2">
-            <th>設定時間</th>
+            <th className="border-2 px-3">設定時間</th>
+            <th className="border-2 px-3">削除</th>
           </tr>
         </thead>
         <tbody>
-          {tableValue?.time.map((element) => {
+          {tableValue?.map((element, index) => {
             return (
-              <tr className="border-2">
-                <td>{element}</td>
+              <tr className="border-2" key={index}>
+                <td className="px-2 py-1">{element}</td>
+                <td className="px-2 py-1">
+                  <button
+                    className="rounded-md bg-violet-100/90 p-1 text-base"
+                    onClick={() => onButton(index)}
+                  >
+                    削除
+                  </button>
+                </td>
               </tr>
             );
           })}
-          <tr className="border-2">
-            <td>11:00</td>
-          </tr>
         </tbody>
       </table>
 
